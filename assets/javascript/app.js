41499d44f1fa5e14dddb23d721b55f2095e38736
squall2046 <squall2046@hotmail.com>
2018-12-11 02:35:17 -0500

    GifTastic Version1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,6 @@
 
 var animalList = ["cats", "dogs", "birds", "fishes", "monkeys", "chicks", "pandas", "cranes", "tigers", "mantis", "snakes", "turtles", "mooses", "lizards", "alpacas", "beavers", "owls", "bears", "wolfs", "horses", "deers"]
 
-////////////////////////////////////////////////
-//click or enter search-bar to create new button:
 function generatebutton() {
     for (var i = 0; i < animalList.length; i++) {
         $(".gif-buttons-default").append("<button type='submit' class='newbutton-js'>" + animalList[i] + "</button> ");
@@ -11,42 +9,60 @@ function generatebutton() {
 }
 generatebutton();
 
+////////////////////////////////////////////////
+//click or enter search-bar to create new button:
 $("#gif-submit").on("click", function () {
+    event.preventDefault();
+
     var newAnimal = $(".gif-input").val().trim();
-    animalList.push(newAnimal);
+    if (newAnimal.length !== 0) {
+        animalList.push(newAnimal);
+    }
+    // clear the input content
+    $(".gif-input").val("");
+
+    // clear list before reloop
     $(".gif-buttons-default").empty();
     generatebutton();
 })
 
 /////////////////////////////////////////////////////
 //click button in animal list to show rating and gif:
-
-// document on click, use for the danymic buttons or elements which are created in js
 $(document).on("click", ".newbutton-js", function () {
     $(".gif-show").empty();
 
     var buttonText = $(this).text();
-    // if change to:
-    // var buttonText = $(".newbutton-js").text();
-    // it will load all buttons texts
-    // only use $(this) work, why??????????
-
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=MrCk33YTz6WfMMgxax4KaDxkCzsj5oVU&limit=10";
-    console.log(this)
-    console.log(queryURL)
+    // if buttonText = $(".newbutton-js").text(); it will load all buttons texts
+    // why??????????
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        //https://api.giphy.com/v1/gifs/search?q=deers&api_key=MrCk33YTz6WfMMgxax4KaDxkCzsj5oVU&limit=10
-        //check the above link to see that data is an array
-        //loop for i, to get rating and gif from each value[i]
         for (var i = 0; i < response.data.length; i++) {
-            $(".gif-show").append("<span class='newrating-js'> Rating: " + response.data[i].rating + "</span>");
-            $(".gif-show").append("<img src='" + response.data[i].images.fixed_height_still.url + "' class='newgif-js'>");
+            // it's better to create <div>, and every <div> will hold a pair of gif and rating (use to float left in css):
+            var gifHolder = $("<div>").addClass("rate-and-gif");
+            var gifstill = response.data[i].images.fixed_height_still.url;
+            var gifstart = response.data[i].images.fixed_height.url;
 
+            gifHolder.append("<img src='" + gifstill + "' data-still='" + gifstill + "' data-animate='" + gifstart + "' data-state='still' class='newgif-js'>");
+            gifHolder.append("<p class='newrating-js'> Rating: " + response.data[i].rating.toUpperCase() + "</p>");
+            $(".gif-show").append(gifHolder)
         }
+
+        $(document).on("click", ".newgif-js", function () {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        })
     });
 })
+
+
 
