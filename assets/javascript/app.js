dbc1643edc80d2b5d7d065235715b2b54f5bcbf7
squall2046 <squall2046@hotmail.com>
2018-12-21 20:54:01 -0500

    color changed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,8 +8,6 @@ function generatebutton() {
 }
 generatebutton();
 
-// $(".gif-input").value('<input type="submit" value="submit" class="btn btn-outline-secondary" id="gif-submit">')
-
 //////////////////////////////
 //click navbar to reload page:
 //////////////////////////////
@@ -35,23 +33,29 @@ $("#gif-submit").on("click", function () {
     // clear list before reloop
     $(".gif-buttons-default").empty();
     generatebutton();
+    favoritePagebutton = $(".gif-buttons-default").prepend("<button type='submit' class='favpageButton-js'> My Favorites </button> ");
 })
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //click animal button from list, then generate more-gif-button, 10 gifs, rating, and favorite button into the EMPTY BOX:
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var num = 10;
-//  DO NOT put: var num=10; into click object, so it won't reload when click. Make number as universal.
+
+// set loading img (from local)
+// var img = $("<img>");
+// img.attr("src", "assets/images/anigif-r.gif");
+
+// DO NOT var apiLimit=10 into function, or it won't reload when click more-gif-button. Make apiLimit as universal.
+var apiLimit = 10;
 
 $(document).on("click", ".newbutton-js", function () {
-    // why if buttonText = $(".newbutton-js").text(); it will load all buttons texts???????????
     $(".gif-show").empty();
     buttonText = $(this).attr("data-name");
+    // why if buttonText = $(".newbutton-js").text(); it will load all buttons texts???????????
     // Use buttonText = $(this).attr("data-name"), instead of var buttonText = $(this).attr("data-name");
     // it means create a variable in Global(equal to this variable is outside the function), not in Local(this variable only be readed inside of this function due to the javascript scope reason).
-    // So, in line80, the queryURL still can read the buttonText variable inside of this function.
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=animal+" + buttonText + "&api_key=MrCk33YTz6WfMMgxax4KaDxkCzsj5oVU&limit=" + num;
-
+    // So, in line91, the queryURL still can read the buttonText variable inside of this function.
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=animal+" + buttonText + "&api_key=MrCk33YTz6WfMMgxax4KaDxkCzsj5oVU&limit=" + apiLimit.toString();
+    console.log(queryURL)
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -62,16 +66,23 @@ $(document).on("click", ".newbutton-js", function () {
                 var gifHolder = $("<div>").addClass("rate-and-gif");
 
                 // 2. append <img> of GIF when click button
-                var gifstill = response.data[i].images.fixed_height_still.url;
-                var gifstart = response.data[i].images.fixed_height.url;
-                gifHolder.append("<img src='" + gifstill + "' data-still='" + gifstill + "' data-animate='" + gifstart + "' data-state='still' class='newgif-js'>");
+                var img = $("<img>");
+                img.addClass("newgif-js");
+                img.attr({ "src": response.data[i].images.fixed_height_still.url, "data-still": response.data[i].images.fixed_height_still.url, "data-animate": response.data[i].images.fixed_height.url, "data-state": "still" });
+                // approach 1: Set multiple attributes in an object(easier).
+                // approach 2: Set multiple attributes one by one:
+                // img.attr("src", response.data[i].images.fixed_height_still.url);
+                // img.attr("data-still", response.data[i].images.fixed_height_still.url);
+                // img.attr("data-animate", response.data[i].images.fixed_height.url);
+                // img.attr("data-state", "still");
+                gifHolder.append(img);
 
                 // 3. append <p> of RATING when click button
                 gifHolder.append("<p class='newrating-js'>" + buttonText + " / Rating: " + response.data[i].rating.toUpperCase() + "</p>");
 
-                // 4. append <i> of STAR BUTTON (favorite)
-                var favoriteButton = '<i class="fas fa-star favme"></i>'
-                gifHolder.append(favoriteButton)
+                // 4. append <i> of STAR Symbol (favorite)
+                var favStar = '<i class="fas fa-star favme"></i>'
+                gifHolder.append(favStar)
 
                 // 5. show gif, rating, and star button in empty box
                 $(".gif-show").prepend(gifHolder)
@@ -88,9 +99,10 @@ $(document).on("click", ".newbutton-js", function () {
 /////////////////////////////////////////////////////////
 $(document).on("click", ".more-gif-button", function () {
     $(".gif-show").empty();
-    num += 10;
-    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=MrCk33YTz6WfMMgxax4KaDxkCzsj5oVU&limit=" + num;
-    console.log(num)
+    apiLimit += 10;
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=animal+" + buttonText + "&api_key=MrCk33YTz6WfMMgxax4KaDxkCzsj5oVU&limit=" + apiLimit.toString();
+    // buttonText will load from line48
+    console.log(apiLimit)
     console.log(queryURL)
     console.log(buttonText);
 
@@ -100,37 +112,20 @@ $(document).on("click", ".more-gif-button", function () {
     }).then(function (response) {
         for (var i = 0; i < response.data.length; i++) {
             if (response.data[i].rating !== "r" && response.data[i].rating !== "pg-13") {
-                // 1. create <div>, and every <div> will hold every pair of gif, rating, and star button:
                 var gifHolder = $("<div>").addClass("rate-and-gif");
-
-                // 2. append <img> of GIF when click button
                 var gifstill = response.data[i].images.fixed_height_still.url;
                 var gifstart = response.data[i].images.fixed_height.url;
                 gifHolder.append("<img src='" + gifstill + "' data-still='" + gifstill + "' data-animate='" + gifstart + "' data-state='still' class='newgif-js'>");
-
-                // 3. append <p> of RATING when click button
                 gifHolder.append("<p class='newrating-js'>" + buttonText + " / Rating: " + response.data[i].rating.toUpperCase() + "</p>");
-
-                // 4. append <i> of STAR BUTTON (favorite)
-                var favoriteButton = '<i class="fas fa-star favme"></i>'
-                gifHolder.append(favoriteButton)
-
-                // 5. show gif, rating, and star button in empty box
+                var favStar = '<i class="fas fa-star favme"></i>'
+                gifHolder.append(favStar)
                 $(".gif-show").prepend(gifHolder)
             }
         }
-        //6. generate a More Button at the top of box (put it out of loop, javascript will read brackets from outside to inside)
         var moreGifButton = '<button class="more-gif-button"><i class="fas fa-caret-square-down"></i></button>'
         $(".gif-show").append(moreGifButton);
     })
-})//////////////////more-gif-button//////////////////////////
-
-//////////////////////////////////////////////
-//click favorite button, inside the EMPTY BOX:
-//////////////////////////////////////////////
-$(document).on("click", ".favme", function () {
-    $(this).toggleClass('active');
-});
+})//---------more-gif-button end-----------
 
 //////////////////////////////////////////////////////
 //click gif to still or animate, inside the EMPTY BOX:
@@ -144,3 +139,20 @@ $(document).on("click", ".newgif-js", function () {
         $(this).attr("data-state", "still");
     }
 });
+
+//////////////////////////////////////////////
+//click favorite button, inside the EMPTY BOX:
+//////////////////////////////////////////////
+$(document).on("click", ".favme", function () {
+    $(this).toggleClass('star-color', function () {
+        // localStorage.setItem("favorite", JSON.stringify());
+
+    });
+});
+
+// favorite button and page
+var favoritePagebutton = $(".gif-buttons-default").prepend("<button class='favpageButton-js'>&bigstar; All My Favorites </button>")
+$(".favpageButton-js").on("click", function () {
+    $(".gif-show").empty();
+    $(".gif-show").append('<span class="guide-word">Hmm, I cannot find any favorite gifs...</span><img src="assets/images/anigif.gif" class="gif-background" alt="gif">')
+})
